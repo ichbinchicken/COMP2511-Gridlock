135d87ee53cdcedf32bd9faaddb94974de8b61fd
Micha <Micha@DESKTOP-518D9V7>
2018-05-17 08:01:49 +1000

    Fixed overwriting issue with last push

--- BoardState.java
+++ BoardState.java
@@ -9,14 +9,15 @@ import java.util.*;
 
 public class BoardState {
 	private ArrayList<Integer> arr; 
+	
 	private int n;
 	
-	int GoalC = n-1;
-	int GoalR = 3;
-	
+	int Cmax = 6;
+	int Rmax = 6;
 
-	private static final int carProb =20;
-	private static final int verProb =90;
+	int GoalC = Cmax-1;
+	int GoalR = 2;
+	
 
 	private static final int  RED =34;
 	private static final int  HCAR=19; //Horizontal sliding
@@ -39,38 +40,29 @@ public class BoardState {
 		return arr;
 	}
 	
-	public void GenSolution() {
+	
+	//In general - hardest solutions roughly ~13, 80, 60
+	public void GenSolution(int maxCars, int carProb, int verProb) {
+
 		Random rand = new Random();
-		int maxCars = rand.nextInt(5)+6; //From 6 to 11 cars
+		int type;
+		//int maxCars = rand.nextInt(5)+6; //From 6 to 11 cars
 		int carCount;
-		int blockers = rand.nextInt(6); //Must always have at least one vertical car near exit
 		int r=0,c;
-		blockers++;
+		//blockers++;
 		arr = new ArrayList<Integer>(Collections.nCopies(n*n, 0));
 		arr.set(RCtoI(GoalR,GoalC), RED);
 		arr.set(RCtoI(GoalR,GoalC-1), RED);
-		arr.set(RCtoI(GoalR,GoalC-2), RED);
-		arr.set(RCtoI(GoalR,GoalC-3), RED);
-
-		int type = VCAR;
-		if(blockers>3) {
-			r=GoalR+1;
-			//Type
-			int r2 = rand.nextInt(1);
-			if(r2==1) {
-				type = VTRUCK;
-			}
-		}
-		c=GoalC-blockers%3;
-		addCar(r,c,type);
-		carCount=2;
+		arr.set(RCtoI(GoalR,GoalC-2), RED); //The space to left of red car must be empty
+		
+		carCount=1;
 		int j=0;
-		while(j<100 && carCount < maxCars) {
+		while(j<100 && carCount <= maxCars) {
 			type=1;
-			r = rand.nextInt()%(n-1);
-			c = rand.nextInt()%(n-1);
-			int r1 = rand.nextInt()%(100);
-			int r2 = rand.nextInt()%100;
+			r = rand.nextInt(5);
+			c = rand.nextInt(5);
+			int r1 = rand.nextInt(101);
+			int r2 = rand.nextInt(101);
 			if(r1>carProb) {
 				type+=2;
 			}
@@ -83,7 +75,7 @@ public class BoardState {
 			j++;
 		}
 		arr.set(RCtoI(GoalR,GoalC-2), 0);
-		arr.set(RCtoI(GoalR,GoalC-3), 0);
+		//arr.set(RCtoI(GoalR,GoalC-3), 0);
 
 		
 	}
@@ -101,7 +93,7 @@ public class BoardState {
 		}
 		
 		if(type == HCAR) {
-			if(c+1<n) {
+			if(c+1<Cmax-1) {
 				if(arr.get(RCtoI(r,c)) ==0 && 0==arr.get(RCtoI(r,c+1))){
 					arr.set(RCtoI(r,c), HCAR);
 					arr.set(RCtoI(r,c+1), HCAR);
@@ -110,7 +102,7 @@ public class BoardState {
 			}
 		}
 		if(type == HTRUCK) {
-			if(c+2<n) {
+			if(c+2<Cmax-1) {
 				if(arr.get(RCtoI(r,c)) ==0 && arr.get(RCtoI(r,c+2))==0 && arr.get(RCtoI(r,c+1))==0){
 					arr.set(RCtoI(r,c), HTRUCK);
 					arr.set(RCtoI(r,c+1), HTRUCK);
@@ -120,7 +112,7 @@ public class BoardState {
 			}
 		}
 		if(type == VTRUCK) {
-			if(r+2<n) {
+			if(r+2<Rmax-1) {
 				if(arr.get(RCtoI(r,c)) ==0 && arr.get(RCtoI(r+1,c))==0 && arr.get(RCtoI(r+2,c))==0){
 					arr.set(RCtoI(r,c), VTRUCK);
 					arr.set(RCtoI(r+1,c), VTRUCK);
@@ -131,7 +123,7 @@ public class BoardState {
 		}
 		
 		if(type == VCAR) {
-			if(r+1<n) {
+			if(r+1<Rmax-1) {
 				if(arr.get(RCtoI(r,c))==0 && arr.get(RCtoI(r+1,c))==0) {
 					arr.set(RCtoI(r,c), VCAR);
 					arr.set(RCtoI(r+1,c), VCAR);
@@ -142,7 +134,7 @@ public class BoardState {
 		return false;
 	}
 	private int RCtoI(int r, int c) {
-		return r * n + c;
+		return r * (Cmax) + c;
 	}
 	
 	
--- SearchTest.java
+++ SearchTest.java
@@ -1,6 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
+import java.util.concurrent.*;
 
 import org.junit.Test;
 
@@ -34,25 +37,20 @@ public class SearchTest {
 							 0, 4, 7, 0,28,28,
 							 0, 4,31,31,32,32};
 		
-
 		
 		
 		
-
 		Search search = new Search();
 		state.GivenBoard(array1);
 		int moves = search.SearchBoard(state);
 		System.out.println("Puzzle Solved in " + moves +" moves");
-
 		
-
 		
 	}
 	
 	@Test
 	public void testCantSolve() {
 		Search search = new Search();
-
 		Integer[] array3 ={	01,19,19,21,21,21,
 				01,22,22,23,23,23,
 				00,34,34,12,15,18,
@@ -90,7 +88,6 @@ public class SearchTest {
 	@Test
 	public void GenCompSimpler() {
 		System.out.println("//GEN");
-
 		//1 is for vert car
 		//3 vert truck
 		//19 horiz car
@@ -118,23 +115,27 @@ public class SearchTest {
 			}
 		}
 		System.out.println("ENDGEN\\\\");
-
 	}*/
 	
 	/*@Test
 	public void BoardGen() {
 		System.out.println("DEVBOARD\\\\");
-
+		int cars=13;
+		int carProb = 80; 
+		int verProb = 60;
 		BoardState state = new BoardState(6);
-		state.GenSolution();
+		//state.GenSolution();
 		//state.printBoard();
 		Search search = new Search();
 		ArrayList<Integer> arr=null;
 		int j=0;
-		while(arr==null) {
-			state.GenSolution();
-			arr = search.GenBoard(state,10);
-			j++;
+		int moves;
+		//while(arr==null) {
+		for(j=0;j<100;j++) {
+			state.GenSolution(cars,carProb,verProb);
+			moves = search.GenBoard(state,10);
+			//j++;
+			System.out.println("Puzzle Gen  " + moves +" moves");
 		}
 		System.out.println("J = "+j);
 		if(arr!=null) {
@@ -148,26 +149,92 @@ public class SearchTest {
 			}
 		}
 		System.out.println("ENDBOARD\\\\");
-
 	}*/
 	
 	@Test
 	public void ThreadTester() {
-		BoundedQueue<BoardState> queue = new BoundedQueue<BoardState>(10);
-		Runnable run1 = new GenThread(queue,5);
-		Runnable run2 = new GenThread(queue,5);
-	    Thread thread1 = new Thread(run1);
-	    Thread thread2 = new Thread(run2);
-
-	    thread1.start();
-	    thread2.start();
-	      
-	    
-	    while(!queue.isFull()) {
-	    	
-	    }
+		int NumDifficulties=5;
+		ArrayList<BoundedQueue<NodeState>> queueList = new ArrayList<BoundedQueue<NodeState>>(5);
+		BoundedQueue<NodeState> queue=null;
+		
+		for(int i=0;i<NumDifficulties;i++) {
+			queue = new BoundedQueue<NodeState>(10);
+			queueList.add(queue);
+		}
+		int numThreads=5;
+		
+		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+		for(int i=0;i<numThreads;i++) {
+			Runnable run = new GenThread(queueList,10, 6, 2);
+			executor.execute(run);
+		}
+        executor.shutdown();
+        while(!executor.isTerminated()) {
+        	executor.isTerminated();
+        }
+        queue = queueList.get(NumDifficulties-1);
+        NodeState node = queue.remove();
+        node.printBoard();
 	}
+	
+	
+/*	@Test
+	public void BoardGen() throws FileNotFoundException {
+		//System.out.println("DEVBOARD\\\\");
+       // PrintWriter writer = new PrintWriter("BoardGen.csv");
+		BoardState state = new BoardState(6);
+		Search search = new Search();
+		ArrayList<Integer> arr=null;
+		ArrayList<Integer> result = new ArrayList<Integer>(Collections.nCopies(50, 0));
+		int j=0;
+		int moves;
+		int cars=12;
+		int carProb = 80; 
+		int verProb = 60;
+		//System.out.println(cars+","+ carProb + " ," + verProb +"," + "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]");
+		int big10 =0;
+		int big15 = 0;
+		int big20 = 0,big25=0,big30=0;;
+		//writer.println("Cars,CProb,VProb,0,1,2,3,4,5,6,7,8,9");
+		//for(cars=6;cars<15;cars++) {
+		//	for(carProb=50;carProb<=90;carProb +=10) {
+				for(verProb=60;verProb<=90;verProb +=10) {
+					big10=0;big15=0;big20=0;big25=0;big30=0;
+					for(j=0;j<10000;j++) {
+						state.GenSolution(cars,carProb,verProb);
+						moves = search.GenBoard(state,0);
+						if(moves>=10) {
+							big10++;
+						}
+						if(moves>=15) {
+							big15++;
+						}
+						if(moves>=20) {
+							big20++;
+						}
+						if(moves>=25) {
+							big25++;
+						}
+						if(moves>=30) {
+							big30++;
+						}
+						int get = result.get(moves);
+						result.set(moves, get+1);
+						
+					}
+					System.out.println(cars+","+ carProb + " ," + verProb +"," + result);
+					//writer.println(result);
+					result = new ArrayList<Integer>(Collections.nCopies(50, 0));
+					
+					System.out.println("\n\nVerProp" + verProb+ " Big10: " + big10 + " Big15: " + big15 + " Big20: " +big20+ " Big25: " +big25+ " Big30: " +big30);
+				}
+		//	}
+			
+		//}
+		//writer.close();
+		//System.out.println("ENDBOARD\\\\");
+	}**/
+	
+	
 
 }
-
-
