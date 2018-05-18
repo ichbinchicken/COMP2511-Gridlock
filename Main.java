c5659e1fece6c5af97636244d311081c0a7f08b7
Michael Hamilton <michael-hamilton@outlook.com.au>
2018-05-18 19:40:03 +1000

    Add Main - not sure why not prev included

---
+++ Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    	
+        GameEngine engine = new GameEngine();
+        Screen screen = new BoardScreen(primaryStage,engine);
+        screen.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        System.exit(0); //Close all threads when window is closed
+
+    }
+}
